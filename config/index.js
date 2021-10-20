faffa0e126bd1ef908a31f9756f73ff29e473809
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2021-10-19 22:57:34 -0300

    Adding script for production

--- config/index.js
+++ config/index.js
@@ -1,2 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+} else {
+  console.log('PRODUCTION');
+}
 require('./../services/mongoose');
