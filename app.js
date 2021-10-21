0946289efa87a30674b85feede17ae553c76eff3
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2021-10-21 01:14:37 -0300

    Adding cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ require('./config');
 
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 const notfound = require('./middleware/404');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', routes);
 
